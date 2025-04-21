05593a3911dc0fa1704255d941c63fb1d2425358
inamulmd <mdinamulh04@gmail.com>
2025-04-21 23:47:40 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState ,useEffect} from 'react';
+import Spinner from "../components/Spinner";
+import Product from "../components/Product"
+const Home = () => {
+    const API_URL = "https://fakestoreapi.com/products";
+    const [loading , setloading] = useState(false);
+    const [posts, setPosts] = useState([]);
+     
+    async function fetchProductData() {
+         setloading(true);
+
+         try{
+           const res = await fetch(API_URL);
+           const data = await res.json(); 
+
+           setPosts(data);
+
+         }
+         catch(error){
+              console.log("Error aagya ji");
+              setPosts([]);
+         }
+         setloading(false);
+    }
+
+    useEffect( () => {
+        fetchProductData();
+    },[])
+  return (
+    <div>
+        {
+            loading ? <Spinner/> :
+            posts.length > 0 ?
+            (<div>
+               {
+                posts.map ( (post) => (
+                    <Product key ={post.id} post={post}/>
+                ))
+               }
+            </div>) :
+            <div>
+              <p>No Data found</p>
+            </div>
+        }
+    </div>
+  )
+}
+
+export default Home
